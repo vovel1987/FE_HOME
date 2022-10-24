4a5194d958a4e9fa03eba331eb9bf175d10d5879
Vladimir <???_?????>
2022-10-24 15:10:50 +0200

    modify

--- HW18.10/javascript.js
+++ HW18.10/javascript.js
@@ -1,35 +1,39 @@
+function camelize(string) {
+  let stringNew = string.split("-");
+  for (let i = 0; i < stringNew.length; i++) {
+    stringNew[i] = stringWithUppercase(stringNew[i]);
+  }
+  let joinString = stringNew.join("");
+  return joinString;
+}
 
-let newString = function (string) {
-    let stringNew = string.split("-");
-    console.log(stringNew);
-    let stringJoin = stringNew
-      .join("")
-      .replace("short", "Short")
-      .replace("string", "String");
-    return stringJoin;
-  };
-  console.log(newString("my-short-string"));
-  
-  let array = [1, -2, 3, 4, -9, 6];
-  let newArrays = function (array) {
-    let positivNumber = 0;
-    let positivArray = [];
-    let negativNumber = 0;
-    let negativArray = [];
-    let newArray = [];
-    for (let i = 0; i < array.length; i++) {
-      if (array[i] > 0) {
-        positivNumber += array[i];
-        positivArray = [positivNumber];
-      }
-      if (array[i] < 0) {
-        negativNumber += array[i];
-        negativArray = [negativNumber];
-      }
+function stringWithUppercase(string) {
+  return string.slice(0, 1).toUpperCase() + string.slice(1);
+}
+console.log(camelize("my-short-string"));
+console.log(camelize("background-color"));
+console.log(camelize("list-style-image"));
+console.log(camelize("-webkit-transition"));
+
+let array = [1, -2, 3, 4, -9, 6];
+let newArrays = function (array) {
+  let positivNumber = 0;
+  let positivArray = [];
+  let negativNumber = 0;
+  let negativArray = [];
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > 0) {
+      positivNumber += array[i];
+      positivArray = [positivNumber];
     }
-    newArray = positivArray.concat(negativArray);
-  
-    return newArray;
-  };
-  console.log(newArrays(array));
-  
+    if (array[i] < 0) {
+      negativNumber += array[i];
+      negativArray = [negativNumber];
+    }
+  }
+  newArray = positivArray.concat(negativArray);
+
+  return newArray;
+};
+console.log(newArrays(array));
--- HW23.10/java.js
+++ HW23.10/java.js
@@ -29,3 +29,19 @@ let newValueMassiv1 = (arr) => {
 console.log(newValueMassiv1(array1));
 console.log(newValueMassiv1(array2));
 console.log(newValueMassiv1(array3));
+
+let massiv = ["hello", "is", 1, 2, 3];
+let nemMasiv = (array) => {
+  let newAr = [];
+  let newSt = [];
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    if (typeof array[i] === "string") {
+      newSt = array[i] + array[i];
+      console.log(newSt);
+      
+    }
+    return (newAr = newAr.concat(newSt));
+  }
+};
+console.log(nemMasiv(massiv));
