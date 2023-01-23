9d7595f0345a8f40d4aa73849fb2334d61740b93
Vladimir <???_?????>
2023-01-23 15:24:48 +0100

    nw

--- HW15.01/index.js
+++ HW15.01/index.js
@@ -2,67 +2,48 @@ const block = document.querySelector(".container");
 const button = document.querySelector(".delete");
 const form = document.querySelector(".form");
 
-//----------------------------------------------------------------------
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const newTitle = title.value;
-  const newDescr = description.value;
-  const newPrice = price.value;
-  const newProduct = {
-    title: newTitle,
-    description: newDescr,
-    price: newPrice,
-  };
-  console.log(newProduct);
-  addPost(newProduct);
-});
 
-const addPost = (post) => {
-  fetch(`https://fakestoreapi.com/products`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(post),
-  })
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      showNewProduct(data);
-      // clearInputs();
-    });
-};
+let products = [];
 
-const showNewProduct = (elem) => {
-  block.innerHTML += `
-     <div class='post'>
-     <p> ${elem.title}</p>
-     <p> ${elem.price}</p>
-      <p>${elem.description} </p>
-     <button class='delete'>Delete</button>
+fetch("https://fakestoreapi.com/products")
+  .then((response) => response.json())
+  .then((product) => {
+    console.log(product);
+    products = [...product];
+    render();
+  });
+
+function render() {
+  block.innerText = "";
+  products.forEach((product) => {
+    const post = document.createElement("div");
+    post.classList.add("post");
+
+    post.innerHTML = `
+    <p>${product.category}</p>
+    <p class='title'>${product.title}</p>
+    <div class='image'>
+    <image src='${product.image}'
+    </div>
+    <p>${product.description}</p>
+    <p>${product.price}</p>
+    <div class="rating">
+               <div class="rate">rate - ${product.rating.rate}</div>
+                <div class="count">count - ${product.rating.count}</div>
      </div>
-  `;
-};
+     <button class="delete">DELETE</button>
 
-function createElement(data) {
-  block.innerHTML = "";
-  data.map((elem) => {
-    block.innerHTML += `
-     <div class='post'>
-     <p> ${elem.title}</p>
-     <p> ${elem.price}</p>
-      <p>${elem.description} </p>
-     <button class='delete'>Delete</button>
-     </div>
-  `;
+    `;
+
+    block.append(post);
+    const button = post.querySelector(".delete");
+
+    button.addEventListener("click", () => removeProduct(product));
   });
 }
 
-const fetchData = () => {
-  fetch(`https://fakestoreapi.com/products`)
-    .then((resp) => resp.json())
-    .then((data) => createElement(data));
-};
-fetchData();
+function removeProduct(product) {
+  products = [...products.filter((elem) => elem.id !== product.id)];
+  render();
+}
